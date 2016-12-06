3a01dd0be0a26599d58dceeaa628f800dd131170
Jonas Nielsen <dev@jonas92.com>
2016-12-06 14:46:55 +0100

    Implement threejs in game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'tjsg-game',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  private THREE: any = require("three-js")();
+  private hostElement: ElementRef;
+  private renderer: any;
+  
+  constructor(el:ElementRef) { 
+    this.hostElement = el
+  }
 
   ngOnInit() {
+    this.renderer = new this.THREE.WebGLRenderer({ alpha: true });
+    this.renderer.setSize(500, 500);
+    this.renderer.setClearColor(0xFFAAAAFF,1);
+    this.hostElement.nativeElement.appendChild(this.renderer.domElement);
+    this.renderer.clear();
   }
 
 }
