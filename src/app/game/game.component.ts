5e3ae1851324edd671b1be5f5cff41fa68965a3e
Jonas Nielsen <dev@jonas92.com>
2016-12-06 18:00:36 +0100

    Demo animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,18 +9,60 @@ export class GameComponent implements OnInit {
 
   private THREE: any = require("three-js")();
   private hostElement: ElementRef;
+  private intervalId: any;
+
+  private scene: any;
+  private camera: any;
   private renderer: any;
+
+  private windowWidth: number = 500;
+  private windowHeight: number = 500;
+  private fps: number = 60;
+
+  private cube: any;
   
   constructor(el:ElementRef) { 
-    this.hostElement = el
+    this.hostElement = el;
   }
 
   ngOnInit() {
+    this.scene = new this.THREE.Scene();
+    this.camera = new this.THREE.PerspectiveCamera( 75, this.windowWidth/this.windowHeight, 0.1, 1000);
     this.renderer = new this.THREE.WebGLRenderer({ alpha: true });
-    this.renderer.setSize(500, 500);
-    this.renderer.setClearColor(0xFFAAAAFF,1);
-    this.hostElement.nativeElement.appendChild(this.renderer.domElement);
+    this.renderer.setSize(this.windowWidth, this.windowHeight);
+    this.renderer.setClearColor('#DDF', 1);
+    this.hostElement.nativeElement.querySelector('#game-container').appendChild(this.renderer.domElement);
     this.renderer.clear();
+    let geometry = new this.THREE.BoxGeometry( 1, 1, 1 );
+    let material = new this.THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.cube = new this.THREE.Mesh( geometry, material );
+    this.scene.add( this.cube );
+
+    this.camera.position.z = 5;
+    this.intervalId = setInterval(() => { this.render(); }, (1/this.fps)*1000);
+  }
+
+  private render() {
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  public setFPS(fps: number) {
+    this.fps = fps;
+    clearTimeout(this.intervalId);
+    this.intervalId = setInterval(() => { this.render(); }, (1/this.fps)*1000);
+  }
+
+  public setSize(width: number, height: number) {
+    this.windowWidth = width;
+    this.windowHeight = height;
+    this.renderer.setSize(this.windowWidth, this.windowHeight);
+  }
+
+  public setBgColor(color: string) {
+    this.renderer.setClearColor(color, 1);
   }
 
 }
